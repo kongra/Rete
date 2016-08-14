1f9f92619cd381bba3ad2051625c47013712a58e
Konrad Grzanek <kongra@gmail.com>
2016-08-14 19:24:55 +0200

    Changed deps.

--- rebuild.sh
+++ rebuild.sh
@@ -4,7 +4,7 @@ rm -rf .cabal-sandbox
 rm -rf dist
 
 cabal sandbox init
-cabal sandbox add-source ../kask-base
-cabal sandbox add-source ../treeprint
+cabal sandbox add-source ../01-kask-base
+cabal sandbox add-source ../02-treeprint
 
 cabal install -j
